361744348bbef0df2c7d752b93a8d763d10528c6
Sanchezdav <sanchez.dav90@gmail.com>
2014-03-23 03:08:42 -0600

    Primer Commit
    Se creo proyecto con multiples settings y multiples requirements
    Se creo la app de home
    Se instalo South y unipath
    Se creo la base de datos con Postgresql

---
+++ SistemaDiscusiones/SistemaDiscusiones/settings/base.py
@@ -0,0 +1,49 @@
+from unipath import Path
+
+BASE_DIR = Path(__file__).ancestor(3)
+
+SECRET_KEY = 'a9xcbii$8bx!k3krtg+92v2vt5o+=l14b2)bda-#1_tr)(2%9)'
+
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+THIRD_PARTY_APPS = (
+    'south',
+)
+
+LOCAL_APPS = (
+
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'SistemaDiscusiones.urls'
+
+WSGI_APPLICATION = 'SistemaDiscusiones.wsgi.application'
+
+LANGUAGE_CODE = 'es-mx'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+TEMPLATE_DIRS = [BASE_DIR.child('templates')]
---
+++ SistemaDiscusiones/SistemaDiscusiones/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Discusiones',
+        'USER': 'David',
+        'PASSWORD': '8608',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = '/static/'
